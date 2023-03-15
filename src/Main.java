9ff5aafe65d657799914ab71a1f46bade2fd275c
Kronbergs <kronbergseva@gmail.com>
2023-03-15 08:09:29 +0200

    Izveidots Log in/Sign up lodziņš

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,31 @@
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
 
 public class Main {
 	public static void main(String[] args) {
-
+		JFrame jf = new JFrame("Reģistrācijas logs");
+		JTextField loginLogs = new JTextField();
+		JLabel loginVards = new JLabel("Lietotājvārds:");
+		JTextField paroleLogs = new JTextField();
+		JLabel paroleVards = new JLabel("Parole:");
+		JButton logIn = new JButton("Ieiet");
+		JButton signUp = new JButton("Pieslēgties");
+		loginVards.setBounds(10,70,150,30);
+		loginLogs.setBounds(150,70,170,30);
+		paroleVards.setBounds(10,110,150,30);
+		paroleLogs.setBounds(150,110,170,30);
+		logIn.setBounds(55,180,130,30);
+		signUp.setBounds(220,180,130,30);
+		jf.add(loginVards);
+		jf.add(loginLogs);
+		jf.add(paroleVards);
+		jf.add(paroleLogs);
+		jf.add(logIn);
+		jf.add(signUp);
+		jf.setSize(400,400);
+		jf.setLayout(null);
+		jf.setVisible(true);
 	}
 }
